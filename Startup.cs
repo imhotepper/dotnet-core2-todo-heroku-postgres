f37ee29dc8861d43881fa540eb53d940213e54f8
daiot <stefanescu.dragos@gmail.com>
2017-11-21 22:46:38 +0200

    custom pgsql connections tring

--- Startup.cs
+++ Startup.cs
@@ -24,12 +24,37 @@ namespace app2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
- var enumerator = Environment.GetEnvironmentVariables();
-            
-var connectionString = enumerator["DATABASE_URL"]?.ToString();
-if (string.IsNullOrWhiteSpace(connectionString)) connectionString = Configuration.GetConnectionString("DbCtx");
-          
-     services.AddEntityFrameworkNpgsql().AddDbContext<DbCtx>(options => options.UseNpgsql(connectionString));
+
+
+            var enumerator = Environment.GetEnvironmentVariables();
+
+
+
+            var connectionString = string.Empty;// enumerator["DATABASE_URL"]?.ToString();
+
+            //Get Database Connection 
+            string _connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+            if (!string.IsNullOrWhiteSpace(_connectionString))
+            {
+                _connectionString.Replace("//", "");
+
+                char[] delimiterChars = { '/', ':', '@', '?' };
+                string[] strConn = _connectionString.Split(delimiterChars);
+                strConn = strConn.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+                var user = strConn[1];
+                var pass = strConn[2];
+                var server = strConn[3];
+                var database = strConn[5];
+                var port = strConn[4];
+                connectionString = "host=" + server + ";port=" + port
+                + ";database=" + database + ";uid=" + user
+                + ";pwd=" + pass + ";sslmode=Require;Trust Server Certificate=true;Timeout=1000";
+
+            }
+            if (string.IsNullOrWhiteSpace(connectionString)) connectionString = Configuration.GetConnectionString("DbCtx");
+
+            services.AddEntityFrameworkNpgsql().AddDbContext<DbCtx>(options => options.UseNpgsql(connectionString));
 
             services.AddMvc();
         }
